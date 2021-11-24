95f1c15363852e2867c983536e92a7b51274bc07
Leonardo Martinez <william.martinez.mt@correo.usa.edu.co>
2021-11-24 11:53:53 -0500

    reto1

---
+++ src/main/java/Reto1/UserCrudRepository.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Reto1;
+
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Martinez Huertas
+ */
+public interface UserCrudRepository extends CrudRepository<User, Integer>{
+    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndPassword(String email,String password); 
+   
+    
+}
